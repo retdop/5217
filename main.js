b45f91bf4f4f07e9cf7055ea3798fa930b6ce24e
gabriel <gaboubastard@gmail.com>
2016-05-11 10:45:46 +0200

    MVP

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-function work () {
-  var timeLeft = 57
+function start () {
+  var button = document.getElementsByTagName('button')[0]
+  button.parentNode.removeChild(button)
+  work(5)
+}
+
+function work (timeLeft) {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'blue'
+  document.getElementById('step').innerHTML = 'WORK'
+  document.getElementById('time').innerHTML = timeLeft
   setInterval(function () {
-    document.getElementById('all').style.backgroundColor = 'blue'
-    document.getElementById('time').innerHTML = timeLeft
     timeLeft--
+    document.getElementById('time').innerHTML = timeLeft
+    if (timeLeft === 0) rest(3)
+  }, 1000)
+}
+
+function rest (timeLeft) {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'red'
+  document.getElementById('step').innerHTML = 'REST'
+  document.getElementById('time').innerHTML = timeLeft
+  setInterval(function () {
+    timeLeft--
+    document.getElementById('time').innerHTML = timeLeft
+    if (timeLeft === 0) work(5)
   }, 1000)
 }
